bf60a36d864a4824b5244120c177196e9b81110b
hungnm <hung.nguyenmanh@trustdatas.com>
2023-06-25 10:17:24 +0700

    add validate for role, user group

--- metabucket-application/src/main/java/tech/ibrave/metabucket/application/user/restful/request/group/PersistUserGroupReq.java
+++ metabucket-application/src/main/java/tech/ibrave/metabucket/application/user/restful/request/group/PersistUserGroupReq.java
@@ -2,6 +2,9 @@ package tech.ibrave.metabucket.application.user.restful.request.group;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import tech.ibrave.metabucket.domain.user.dto.UserDto;
@@ -16,10 +19,13 @@ import java.util.List;
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PersistUserGroupReq {
-
-    @NotBlank(message = "{mb.groups.create.name_empty}")
+    @NotBlank(message = "{mb.groups.create.required_groupname}")
+    @Pattern(regexp = "[^`0-9˜!`#$%ˆ&*()_\\-+=|\\{}\\[\\]?/:;\".,<>]{0,32}$",
+            message = "{mb.groups.create.invalid_groupname}")
     private String name;
+    @Size(max = 500, message = "{mb.groups.create.invalid_description}")
     private String description;
-    private boolean enable;
+    @NotNull(message = "{mb.groups.create.required_status}")
+    private Boolean enable;
     private List<UserDto> users;
 }
--- metabucket-application/src/main/java/tech/ibrave/metabucket/application/user/restful/request/role/PersistRoleReq.java
+++ metabucket-application/src/main/java/tech/ibrave/metabucket/application/user/restful/request/role/PersistRoleReq.java
@@ -2,6 +2,9 @@ package tech.ibrave.metabucket.application.user.restful.request.role;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import tech.ibrave.metabucket.domain.user.User;
@@ -18,10 +21,14 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PersistRoleReq {
 
-    @NotBlank(message = "{mb.roles.create.name_empty}")
+    @NotBlank(message = "{mb.roles.create.required_rolename}")
+    @Pattern(regexp = "[^`0-9˜!`#$%ˆ&*()_\\-+=|\\{}\\[\\]?/:;\".,<>]{0,32}$",
+            message = "{mb.roles.create.invalid_rolename}")
     private String name;
+    @Size(max = 500, message = "{mb.roles.create.invalid_description}")
     private String description;
-    private boolean enable;
+    @NotNull(message = "{mb.roles.create.required_status}")
+    private Boolean enable;
     private List<String> permissions;
     private List<User> users;
 }
